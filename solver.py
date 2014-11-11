a33349cf5dfe76243c6efd990752a8ab39a39752
jgoldm01 <jeremy.goldmanmd@gmail.com>
2014-11-11 13:50:02 -0500

    solves simple sudoku puzzles, prints out board as arrays

--- solver.py
+++ solver.py
@@ -7,6 +7,11 @@ board = [];
 
 def main():
 	read_in()
+	eliminate_duplicates()
+	if (isfinished()):
+		print_board();
+	else:
+		nondeterministic_test()
 
 def read_in():
 	index = 0;
@@ -20,6 +25,67 @@ def read_in():
 				index += 1;
 	print board
 
+def eliminate_duplicates():
+	isChanged = False
+	for i in range(9):
+		isChanged = rows(i, isChanged)
+		isChanged = cols(i, isChanged)
+		isChanged = blocks(i, isChanged)
+	if isChanged:
+		set_values()
+		eliminate_duplicates()
+
+def rows(i, isChanged):
+	i *= 9
+	#iterates through row, looking for integers
+	for j in range (i, i+9):
+		if isinstance(board[j], int):
+			#iterates through row, 
+			#eliminating the found number from possibility lists
+			for k in range(i, i+9):
+				if isinstance(board[k], list) and board[j] in board[k]:
+					isChanged = True;
+					board[k].remove(board[j]);
+	return isChanged
+
+def cols(i, isChanged):
+	for j in range(i,81,9):
+		if isinstance(board[j], int):
+			for k in range(i,81,9):
+				if isinstance(board[k], list) and board[j] in board[k]:
+					isChanged = True;
+					board[k].remove(board[j]);
+	return isChanged
+
+def blocks(i, isChanged):
+	s = (i/3)*27 + (i%3)*3;
+	blockArr = [s, s+1, s+2, s+9, s+10, s+11, s+18, s+19, s+20];
+	for j in blockArr:
+		if isinstance(board[j], int):
+			#iterates through row, 
+			#eliminating the found number from possibility lists
+			for k in blockArr:
+				if isinstance(board[k], list) and board[j] in board[k]:
+					isChanged = True;
+					board[k].remove(board[j]);
+	return isChanged
+
+def set_values():
+	for i in xrange(81):
+		if isinstance(board[i], list):
+			if len(board[i]) == 1:
+				board[i] = board[i][0]
+
+def isfinished():
+	return True
+
+def nondeterministic_test():
+	print "this is a hard puzzle"
+
+def print_board():
+	for i in range(9):
+		print board[i*9:i*9+9]
+
 if __name__ == "__main__":
 	main()
 
@@ -29,4 +95,8 @@ if __name__ == "__main__":
 	board[1].remove(1);
 	val = 1;
 	if isinstance(val, int):
-		print board, bol; '''
+		print board, bol; 
+	if 23 in board[0]:
+		board[0].remove(23)
+		len(board[0])
+'''
---
+++ tests.py
@@ -0,0 +1,7 @@
+'''li = [1,2,3];
+i = 1;
+if isinstance(i, list) and 2 in i:
+	i.remove(2)'''
+
+for j in [3,4,65,8]:
+	print len(j);
