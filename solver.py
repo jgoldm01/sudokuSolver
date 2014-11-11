730d5eff37390af7d8c9f65a5dce063a5ba7445d
jgoldm01 <jeremy.goldmanmd@gmail.com>
2014-11-11 14:28:59 -0500

    simple solution completed, about to start adding non-determinism

--- solver.py
+++ solver.py
@@ -23,68 +23,65 @@ def read_in():
 				else:
 					board.append(int(char));
 				index += 1;
-	print board
 
 def eliminate_duplicates():
 	isChanged = False
 	for i in range(9):
-		isChanged = rows(i, isChanged)
-		isChanged = cols(i, isChanged)
-		isChanged = blocks(i, isChanged)
+		isChanged = rcb_elimination(i, isChanged, rowArr)
+		isChanged = rcb_elimination(i, isChanged, colArr)
+		isChanged = rcb_elimination(i, isChanged, blockArr)
 	if isChanged:
 		set_values()
 		eliminate_duplicates()
 
-def rows(i, isChanged):
-	i *= 9
-	#iterates through row, looking for integers
-	for j in range (i, i+9):
-		if isinstance(board[j], int):
-			#iterates through row, 
-			#eliminating the found number from possibility lists
-			for k in range(i, i+9):
-				if isinstance(board[k], list) and board[j] in board[k]:
-					isChanged = True;
-					board[k].remove(board[j]);
-	return isChanged
+def rowArr(i):
+	return range(i*9, (i*9)+9);
 
-def cols(i, isChanged):
-	for j in range(i,81,9):
-		if isinstance(board[j], int):
-			for k in range(i,81,9):
-				if isinstance(board[k], list) and board[j] in board[k]:
-					isChanged = True;
-					board[k].remove(board[j]);
-	return isChanged
+def colArr(i):
+	return range(i,81,9);
 
-def blocks(i, isChanged):
+def blockArr(i):
 	s = (i/3)*27 + (i%3)*3;
-	blockArr = [s, s+1, s+2, s+9, s+10, s+11, s+18, s+19, s+20];
-	for j in blockArr:
+	return [s, s+1, s+2, s+9, s+10, s+11, s+18, s+19, s+20];
+
+def rcb_elimination(i, isChanged, rcb_func):
+	rcbRange = rcb_func(i);
+	for j in rcbRange:
 		if isinstance(board[j], int):
 			#iterates through row, 
 			#eliminating the found number from possibility lists
-			for k in blockArr:
+			for k in rcbRange:
 				if isinstance(board[k], list) and board[j] in board[k]:
 					isChanged = True;
 					board[k].remove(board[j]);
 	return isChanged
 
 def set_values():
-	for i in xrange(81):
+	for i in range(81):
 		if isinstance(board[i], list):
 			if len(board[i]) == 1:
 				board[i] = board[i][0]
 
 def isfinished():
+	for i in range(81):
+		if isinstance(board[i], list):
+			return False
 	return True
 
 def nondeterministic_test():
 	print "this is a hard puzzle"
 
 def print_board():
-	for i in range(9):
-		print board[i*9:i*9+9]
+	index = 0;
+	for i in range(81):
+			print board[i],
+			if index%9 == 2 or index%9 == 5:
+				print "|",
+			elif index%9 == 8:
+				print "\n",
+			if index%27 == 26:
+				print "_ _ _ _ _ _ _ _ _ _ _ "
+			index += 1;
 
 if __name__ == "__main__":
 	main()
--- tests.py
+++ tests.py
@@ -3,5 +3,14 @@ i = 1;
 if isinstance(i, list) and 2 in i:
 	i.remove(2)'''
 
-for j in [3,4,65,8]:
-	print len(j);
+'''for j in [3,4,65,8]:
+	print len(j);'''
+
+it = iter(range(6))
+for x, y, z in zip(it, it, it):
+	print x, y, z, "|"
+
+for f in range(6):
+	print f,
+
+print "\n_ _ _ _ _ _ "
